a591229e52925598ca69f8b2657378233a6c7c68
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-09-22 18:53:01 -0300

    modules 1

---
+++ NodeJS Modules/app.js
@@ -0,0 +1,8 @@
+const {Person} = require('./mod1');
+
+const p1 = new Person('Gabriel');
+console.log(p1);
+
+// const {nameC, surnameC, sayName} = require('./mod1');
+// console.log(nameC, surnameC);
+// console.log(sayName())
---
+++ NodeJS Modules/mod1.js
@@ -0,0 +1,23 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+exports.Person = Person;
+
+// const nameC = 'Gabriel';
+// const surnameC = 'Quintão';
+
+// const sayName = () => nameC + ' ' + surnameC;
+
+// module.exports.nameC = nameC;
+// module.exports.surnameC = surnameC;
+// module.exports.sayName = sayName;
+
+// exports.nameC = nameC;
+// exports.surnameC = surnameC;
+// exports.sayName = sayName;
+// this.something = 'something';
+
+// console.log(exports);
