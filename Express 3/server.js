deb88614645cf4120320cd1b38984762780bc069
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-09-26 13:43:20 -0300

    req.params, req.query, and req.body

--- Express 3/server.js
+++ Express 3/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
 app.get('/', (req, res) => {
     res.send(`<form action="/" method="POST">
     
@@ -10,8 +16,15 @@ app.get('/', (req, res) => {
     </form>`);
 });
 
+app.get('/tests/:idUsers?', (req, res) => {
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.params);
+});
+
 app.post('/', (req, res) => {
-    res.send('Sent.')
+    console.log(req.body);
+    res.send(`You sent me: ${req.body.name}`)
 });
 
 app.listen(3000, () => {
